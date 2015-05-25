ecb4b200faf904717b801797503b95664c340f1c
Jack Skyworker (home) <jskyworker@gmail.com>
2015-05-25 21:41:01 +0300

    Add first test

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class DrobotTests(unittest.TestCase):
+    """ Tests for simple flask application
+    """
+
+    def test_args(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
