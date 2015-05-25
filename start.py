4af1925fe9de860774ebb268aeff164ee547b3d4
Jack Skyworker (home) <jskyworker@gmail.com>
2015-05-25 21:48:44 +0300

    Small refactoring

--- start.py
+++ start.py
@@ -32,18 +32,22 @@ def post():
     data = json.loads(request.data)
     s = Template('{"date": "$date", "uid": "$uid", "name": "$name"}')
     for i in data:
-        h = hashlib.md5(s.substitute(name=i['name'], date=i['date'], uid=i['uid']).encode('utf-8'))
+        h = hashlib.md5(s.substitute(name=i['name'], date=i['date'],
+                                     uid=i['uid']).encode('utf-8'))
         if h.hexdigest() != i['md5checksum']:
-            return "Provided checksum {0}, should be {1} for name {2}.".format(i['md5checksum'], h.hexdigest(), i['name'])
+            return "Provided checksum {0}, should be {1} for name {2}.".\
+                format(i['md5checksum'], h.hexdigest(), i['name'])
         else:
             if not store_mongo(i):
                 return "Checksum was correct but data was not saved."
-            return "Correct checksum {0} for name {1}".format(h.hexdigest(), i['name'])
+            return "Correct checksum {0} for name {1}".format(h.hexdigest(),
+                                                              i['name'])
 
 
 def get_num(uid, date):
     """
-    Given a uid and a date the endpoint should return the number of occurrences of a given UID (John Doe) for that day.
+    Given a uid and a date the endpoint should return the number of occurrences
+    of a given UID (John Doe) for that day.
     :param uid:
     :param date:
     :return:
@@ -53,7 +57,8 @@ def get_num(uid, date):
     with open('data.json', 'r') as f:
         data = json.load(f)
     for i in data:
-        if date_c == datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S.%f').date() and uid == i['uid']:
+        if date_c == datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S.%f').date() \
+                and uid == i['uid']:
             print("uid {0}, date {1}".format(uid, date))
             count += 1
     return count
@@ -61,20 +66,18 @@ def get_num(uid, date):
 
 def get_mongo(uid, date):
     """
-    Given a uid and a date the endpoint should return the number of occurrences of a given UID (John Doe) for that day.
+    Given a uid and a date the endpoint should return the number of occurrences
+    of a given UID (John Doe) for that day.
     :param uid:
     :param date:
     :return:
     """
     date_c = datetime.strptime(date, '%Y-%m-%d').date()
     count = 0
-    try:
-        client = MongoClient(host='localhost', port=27017)
-        db = client.test
-        cursor = db.posts.find({"uid": uid})
-    except Exception as e:
-        print(str(e))
+    db = get_mongo_db()
+    if not db:
         return -1
+    cursor = db.posts.find({"uid": uid})
     for i in cursor:
         if date_c == datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S.%f').date():
             print("uid {0}, date {1}".format(uid, date))
@@ -89,14 +92,27 @@ def store_mongo(data):
     :return:
         False in case of exception
     """
+    db = get_mongo_db()
+    if not db:
+        return False
+    db.posts.insert(data)
+    return True
+
+
+def get_mongo_db(host='localhost', port=27017):
+    """
+    Function to get Mongo db object.
+    :param host: Host we need to connect to.
+    :param port: Port number
+    :return: db object or False
+    """
     try:
-        client = MongoClient(host='localhost', port=27017)
+        client = MongoClient(host, port)
         db = client.test
-        db.posts.insert(data)
+        return db
     except Exception as e:
         print(str(e))
         return False
-    return True
 
 
 if __name__ == "__main__":
